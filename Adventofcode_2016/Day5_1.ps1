5ba784facca1a7eb1304ec1b1e3157dbdcfd521d
rana-aziz <67806702+rana-aziz@users.noreply.github.com>
2020-11-02 19:26:58 +0100

    Create Day5_1.ps1

---
+++ Adventofcode_2016/Day5_1.ps1
@@ -0,0 +1,33 @@
+$stopwatch=[system.diagnostics.stopwatch]::StartNew()
+
+$puzzle="ugkcyxxp"
+
+function GetHash{
+    param(
+        [int]$integer 
+    )
+do{
+     $someString = $puzzle + $integer
+     $md5 = New-Object -TypeName System.Security.Cryptography.MD5CryptoServiceProvider
+     $utf8 = New-Object -TypeName System.Text.UTF8Encoding
+     $hash = [System.BitConverter]::ToString($md5.ComputeHash($utf8.GetBytes($someString)))
+     $integer++
+}until ($hash.StartsWith("00-00-0"))
+ if($hash -match '00-00-0(.).*'){
+ $cahr = $Matches.1
+ }
+return $integer, $cahr
+
+}
+
+$nextInteger=0
+$Password = ""
+for($i=1; $i-le 8; $i++){
+$nextInteger, $cahr = Gethash -integer $nextInteger
+$Password+=$cahr
+Write-Host "This is round number $i"
+Write-Host "Password so far is $Password"
+Write-Host "Current ineteger is $nextInteger"
+}
+
+Write-Host "Door password is $Password"
